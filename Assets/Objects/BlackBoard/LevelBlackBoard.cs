6b94501394beeabd696e0adbc83c956b77a2d9d1
Biel Rabasa <bielrabasa@gmail.com>
2023-06-17 01:20:30 +0200

    Espills final mecaniques

--- Assets/Objects/BlackBoard/LevelBlackBoard.cs
+++ Assets/Objects/BlackBoard/LevelBlackBoard.cs
@@ -6,4 +6,6 @@ public class LevelBlackBoard : MonoBehaviour
 {
     [Range(1, 3)]
     public int numberOfMaps = 1;
+
+    public float distance = 100.0f;
 }
--- Assets/Objects/Espill/EspillScript.cs
+++ Assets/Objects/Espill/EspillScript.cs
@@ -6,27 +6,72 @@ public class EspillScript : MonoBehaviour
 { 
     [SerializeField]
     float destroyTime = 0.5f;
-
     bool destroying = false;
+
+    public bool copia = false;
+    public EspillScript[] brothers;
 
     private void Start()
     {
-        //Duplicar i connectar objectes depenent del nivell (blackboard)
+        if (copia) return;
+
+        //Duplicar i connectar objectes depenent del nivell
+
+        LevelBlackBoard bb = FindObjectOfType<LevelBlackBoard>();
+
+        if(bb.numberOfMaps == 2)
+        {
+            //Create copies
+            GameObject mirall = Instantiate(gameObject, transform.position + new Vector3(bb.distance, 0.0f, 0.0f), transform.rotation, transform.parent);
+            EspillScript es = mirall.GetComponent<EspillScript>();
+            es.copia = true;
+
+            //Set brothers in all 2 mirrors
+            brothers = new EspillScript[] { es };
+            es.brothers = new EspillScript[] { this };
+        }
+
+        if (bb.numberOfMaps == 3)
+        {
+            //Create copies
+            GameObject mirall1 = Instantiate(gameObject, transform.position + new Vector3(bb.distance, 0.0f, 0.0f), transform.rotation, transform.parent);
+            GameObject mirall2 = Instantiate(gameObject, transform.position + new Vector3(-bb.distance, 0.0f, 0.0f), transform.rotation, transform.parent);
+            
+            //Get scripts
+            EspillScript es1 = mirall1.GetComponent<EspillScript>();
+            EspillScript es2 = mirall2.GetComponent<EspillScript>();
+            es1.copia = true;
+            es2.copia = true;
+
+            //Set brothers in all 3 mirrors
+            brothers = new EspillScript[] { es1, es2 };
+            es1.brothers = new EspillScript[] { this, es2 };
+            es2.brothers = new EspillScript[] { this, es1 };
+        }
     }
 
     void Update()
     {
         if(destroying)
         {
-            float alphaReduction = Time.deltaTime / destroyTime;
-            gameObject.GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, alphaReduction);
-            Debug.Log(gameObject.GetComponent<SpriteRenderer>().color);
+            gameObject.GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, Time.deltaTime / destroyTime);
         }
+    }
+
+    void Disapear()
+    {
+        Destroy(gameObject, destroyTime);
+        if (!destroying) destroying = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject, destroyTime);
-        if (!destroying) destroying = true;
+        Disapear();
+
+        //kill all objects
+        foreach(EspillScript obj in brothers)
+        {
+            obj.Disapear();
+        }
     }
 }
