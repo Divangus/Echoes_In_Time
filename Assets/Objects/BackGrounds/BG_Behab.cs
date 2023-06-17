69c414b31900904e09505b9afe5f77dfa435a7d1
Biel Rabasa <bielrabasa@gmail.com>
2023-06-17 19:55:10 +0200

    BackGround Animations

--- Assets/Objects/BackGrounds/BG_Behab.cs
+++ Assets/Objects/BackGrounds/BG_Behab.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 
 public class BG_Behab : MonoBehaviour
 {
-    private Color light;
+    public Vector2[] path;
+    private Rigidbody2D trail;
+    private int currentStep = 1;
+    private Vector2 reachPos;
+    private bool changePos = true;
 
-    private bool up = false;
+    private SpriteRenderer llum;
+
+    private float angle = 0.0f;
+    private int vel = 35;
+
+    private void Awake()
+    {
+        
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        light = transform.Find("light").gameObject.GetComponent<SpriteRenderer>().color;
+        llum = transform.Find("light").gameObject.GetComponent<SpriteRenderer>();
+        trail = transform.Find("trail").GetComponent<Rigidbody2D>();
+        trail.position = path[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (up)
+        llum.color = new Color(255, 255, 255, 0.4f - Mathf.Sin(angle * Mathf.PI / 180.0f) * 0.35f);
+        angle += Time.deltaTime * vel;
+
+        if (angle > 180.0f)
         {
-            light.a += Time.deltaTime;
-            if (light.a > 0.9f) up = false;
+            angle -= 180.0f;
+            vel = Random.Range(20, 35);
         }
-        else
+
+        //Next Point
+        CheckPathChange();
+
+        //Change Velocity
+        CheckVelocityChange();      
+    }
+
+    void CheckPathChange()
+    {
+        float distance = (reachPos - trail.position).magnitude;
+        //Debug.Log("ReachPos: " + reachPos + "\nDistance: " + distance + "\nPos: " + trail.position);
+        if (distance < 0.2)
         {
-            light.a -= Time.deltaTime;
-            if (light.a < 0.1f) up = true;
+            changePos = true;
+            if (currentStep != (path.Length - 1))
+            {
+                trail.position = path[currentStep];
+                currentStep++;
+            }
+            else
+            {
+                currentStep = 1;
+                trail.position = path[0];
+            }
         }
-        //TODO doesn't change
+    }
+
+    void CheckVelocityChange()
+    {
+        if (changePos)
+        {
+            reachPos = path[currentStep];
+            trail.velocity = (reachPos - trail.position).normalized * Random.Range(3.0f, 6.0f);
+
+            changePos = false;
+        }
     }
 }
