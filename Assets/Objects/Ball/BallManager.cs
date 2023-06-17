cf351f6c86d00fd9606ba41574eeaf91ea90f8a9
MaralGS <algarselma@gmail.com>
2023-06-17 19:42:48 +0200

    Ball Selecction Finished (Have to Be Polished)

--- Assets/Objects/Ball/BallManager.cs
+++ Assets/Objects/Ball/BallManager.cs
@@ -1,64 +1,26 @@
-using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class BallManager : MonoBehaviour
 {
-    public Rigidbody2D ball;
-    public float _ballSpeed;
-    Vector2 GetBallPos;
-    Vector2 GetMousePos;
-    float _angle;
-    bool _ShootedBall;
-
+    public List<GameObject> _balls;
+    public GameObject _SelectedBall;
     // Start is called before the first frame update
     void Start()
     {
-        _ShootedBall = false;
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-        if (hit.collider == gameObject.GetComponent<Collider2D>())
+        for(int i = 0; i < _balls.Count; i++)
         {
-            if (Input.GetMouseButtonDown(0))
+            if(_balls[i] == _SelectedBall)
             {
-                GetBallPos = gameObject.transform.position;
+                _balls[i].GetComponent<BallScript>().CalculatePos();
             }
         }
-
-        if (Input.GetMouseButton(0))
-        {
-            GetMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            CalculatePos();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && _ShootedBall == false)
-        {
-            Rigidbody2D clone_ball = Instantiate(ball, transform.position, transform.rotation) as Rigidbody2D;
-
-            clone_ball.velocity = transform.up * _ballSpeed * Time.deltaTime;
-            _ShootedBall = true;
-        }
-    }
-
-    void CalculatePos()
-    {
-        Vector2 FinalDir = GetMousePos - GetBallPos;
-
-        _angle = Mathf.Atan2(FinalDir.x, FinalDir.y) * 180 / Mathf.PI;
-
-
-        int resolvedAngle = ((int)_angle) / (int)22.5;
-
-        
-
-        transform.rotation = Quaternion.AngleAxis(resolvedAngle * 22.5f, Vector3.back);
     }
 }
---
+++ Assets/Objects/Ball/BallScript.cs
@@ -0,0 +1,69 @@
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D ball;
+    public float _ballSpeed;
+    Vector2 GetBallPos;
+    Vector2 GetMousePos;
+    float _angle;
+    bool _ShootedBall;
+    public bool _SelectedBall;
+    public GameObject _BallManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ShootedBall = false;
+        _SelectedBall = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider == gameObject.GetComponent<Collider2D>())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetBallPos = gameObject.transform.position;
+                _SelectedBall = true;
+                FindObjectOfType<BallManager>()._SelectedBall = this.gameObject;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            GetMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _ShootedBall == false)
+        {
+            Rigidbody2D clone_ball = Instantiate(ball, transform.position, transform.rotation) as Rigidbody2D;
+
+            clone_ball.velocity = transform.up * _ballSpeed * Time.deltaTime;
+            _ShootedBall = true;
+        }
+    }
+
+    public void CalculatePos()
+    {
+        Vector2 FinalDir = GetMousePos - GetBallPos;
+
+        _angle = Mathf.Atan2(FinalDir.x, FinalDir.y) * 180 / Mathf.PI;
+
+
+        int resolvedAngle = ((int)_angle) / (int)22.5;
+
+
+        transform.rotation = Quaternion.AngleAxis(resolvedAngle * 22.5f, Vector3.back);
+
+        //Debug.Log(transform.rotation);
+    }
+}
