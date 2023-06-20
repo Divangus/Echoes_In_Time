3ca97dab27c6979357ee44ad14ce678457fc6e65
Roger Salas <rogersalas2000@gmail.com>
2023-06-20 18:00:31 +0200

    random particles start

--- Assets/Objects/Main_Menu/Particles_Sound.cs
+++ Assets/Objects/Main_Menu/Particles_Sound.cs
@@ -12,13 +12,13 @@ public class Particles_Sound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer_Sparks = 3;
+        timer_Sparks = Random.Range(0.5f, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (timer_Sparks > 0) { timer_Sparks -= Time.deltaTime; }
-        else { timer_Sparks = 3; sparks.Play(); sparks_audio.Play(); }
+        else { timer_Sparks = Random.Range(0.5f, 3); sparks.Play(); sparks_audio.Play(); }
     }
 }
